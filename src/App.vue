9618a451d24e83cc3549b7889c65d2a98fc58dd9
Dan0804 <wheogus185@naver.com>
2022-03-11 22:15:17 +0900

    reactive side menu

--- src/App.vue
+++ src/App.vue
@@ -9,48 +9,49 @@
         <div class="flex flex-col items-start">
           <div class="hover:text-hover_primary hover:bg-lighter px-4 py-2 rounded-full">
               <i class="fa-solid fa-house-chimney fa-fw text-2xl mr-2"></i>
-              <span>홈</span>
+              <span class="hidden lg:inline-block">홈</span>
           </div>
           
           <div class="hover:text-hover_primary hover:bg-lighter px-4 py-2 rounded-full">
               <i class="fa-solid fa-hashtag fa-fw text-2xl mr-2"></i>
-              <span>헤시테그</span>
+              <span class="hidden lg:inline-block">헤시테그</span>
           </div>
           
           <div class="hover:text-hover_primary hover:bg-lighter px-4 py-2 rounded-full">
               <i class="fa-solid fa-bell fa-fw text-2xl mr-2"></i>
-              <span>알람</span>
+              <span class="hidden lg:inline-block">알람</span>
           </div>
           
           <div class="hover:text-hover_primary hover:bg-lighter px-4 py-2 rounded-full">
               <i class="fa-regular fa-envelope fa-fw text-2xl mr-2"></i>
-              <span>쪽지</span>
+              <span class="hidden lg:inline-block">쪽지</span>
           </div>
           
           <div class="hover:text-hover_primary hover:bg-lighter px-4 py-2 rounded-full">
               <i class="fa-regular fa-bookmark fa-fw text-2xl mr-2"></i>
-              <span>북마크</span>
+              <span class="hidden lg:inline-block">북마크</span>
           </div>
           
           <div class="hover:text-hover_primary hover:bg-lighter px-4 py-2 rounded-full">
               <i class="fa-solid fa-list fa-fw text-2xl mr-2"></i>
-              <span>리스트</span>
+              <span class="hidden lg:inline-block">리스트</span>
           </div>
           
           <div class="hover:text-hover_primary hover:bg-lighter px-4 py-2 rounded-full">
               <i class="fa-regular fa-user fa-fw text-2xl mr-2"></i>
-              <span>프로필</span>
+              <span class="hidden lg:inline-block">프로필</span>
           </div>
           
           <div class="hover:text-hover_primary hover:bg-lighter px-4 py-2 rounded-full">
               <i class="fa-solid fa-ellipsis fa-fw text-2xl mr-2"></i>
-              <span>더 보기</span>
+              <span class="hidden lg:inline-block">더 보기</span>
           </div>
         </div>
 
         <div class="w-full h-12">
           <button class="bg-hover_primary text-white w-40 h-12 rounded-full hover:bg-bgdark mt-3">
-            Tweet
+            <span class="hidden lg:block">트윗</span>
+            <i class="fa-solid fa-photo-film lg:hidden"></i>
           </button>
         </div>
       </div>
@@ -60,11 +61,11 @@
         <div class="w-full h-12">
           <button class="px-2 py-1 w-full h-12 rounded-full hover:bg-blue-50 flex items-center">
             <img src="http://picsum.photos/100" class="w-10 h-10 rounded-full" />
-            <div class="ml-2">
+            <div class="lg:ml-2 lg:block hidden">
                 <div class="text-sm font-bold">wheogus185</div>
                 <div class="text-xs text-gray-500 text-left">조대현</div>
             </div>
-            <i class="fa-solid fa-ellipsis fa-fw text-l ml-auto"></i>
+            <i class="fa-solid fa-ellipsis fa-fw text-l ml-auto hidden lg:block"></i>
           </button>
         </div>
       </div>
