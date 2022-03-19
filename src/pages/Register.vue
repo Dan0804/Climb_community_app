d45c5b1b387359fafb3b7e3fcd70daf18b4ef5b4
Dan0804 <wheogus185@naver.com>
2022-03-20 01:03:05 +0900

    finished register process

--- src/pages/Register.vue
+++ src/pages/Register.vue
@@ -1,11 +1,12 @@
 <template>
   <div class="flex flex-col items-center space-y-6 mt-10">
     <i :class="`fa-brands fa-twitter text-4xl text-primary ${loading ? 'animate-bounce' : ''}`"></i>
-    <span class="text-2xl font-bold">Climb Vue 회원가입</span>
+    <span class="text-2xl font-bold">Climb View 회원가입</span>
     <input v-model="username" class="rounded w-96 px-4 py-3 border border-gray-300 focus:ring-2 focus:border-primary" placeholder="아이디" type="text">
     <input v-model="email" class="rounded w-96 px-4 py-3 border border-gray-300 focus:ring-2 focus:border-primary" placeholder="이메일" type="text">
     <input v-model="password" class="rounded w-96 px-4 py-3 border border-gray-300 focus:ring-2 focus:border-primary" placeholder="비밀번호" type="password">
-    <button class="w-96 rounded bg-primary text-white py-4" @click="onRegister">회원가입</button>
+    <button v-if="loading" class="w-96 rounded bg-primary text-white py-4">회원가입 중입니다.</button>
+    <button v-else class="w-96 rounded bg-primary text-white py-4" @click="onRegister">회원가입</button>
     <router-link to="/login">
       <button class="text-primary">계정이 이미 있으신가요? 로그인 하기</button>
     </router-link>
@@ -14,20 +15,38 @@
 
 <script>
 import {ref} from 'vue'
-import { signupEmail } from '../firebase'
+import { signupEmail, db } from '../firebase'
+import { setDoc, doc } from 'firebase/firestore'
+import { useRouter } from 'vue-router'
+
 export default {
   setup() {
     const username = ref('')
     const email = ref('')
     const password = ref('')
     const loading = ref(false)
+    const router = useRouter()
 
     const onRegister = async () => {
       try {
-        const credential = await signupEmail(email.value, password.value)
+        loading.value = true
+        const { user } = await signupEmail(email.value, password.value)
+        await setDoc(doc(db, "users", user.uid), {
+          uid: user.uid,
+          email: email.value,
+          profile_image_url: './profile.jpeg',
+          num_tweets: 0,
+          followers: [],
+          followings: [],
+          created_at: Date.now()
+        })
+        alert('회원가입이 완료되었습니다. 로그인 해주세요.')
+        router.push('/login')
       } catch(e) {
         // console.log('create user with email and password error', e)
         if (e.message === 'Firebase: Password should be at least 6 characters (auth/weak-password).') {alert('비밀번호는 6글자 이상으로 해주세요!')}
+      } finally {
+        loading.value = false
       }
     }
     return {
