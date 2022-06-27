1938dfe444da5582c633198713aa8dc440696cc2
Dan0804 <wheogus185@naver.com>
2022-06-28 07:42:56 +0900

    edit profile

--- src/pages/Profile.vue
+++ src/pages/Profile.vue
@@ -1,37 +1,28 @@
 <template>
-    <div v-if="profileUser" class="flex flex-1 flex-col border-r border-color">
+    <div class="flex flex-1 flex-col border-r border-color">
         <!-- title -->
         <div class="flex px-3 py-0.5 border-b border-color">
             <button @click="router.go(-1)" class="mr-4">
                 <i class="fa-solid fa-arrow-left text-blue-300 rounded-full p-3 hover:bg-blue-50"></i>
             </button>
             <div class="-space-y-1.5">
-                <div class="font-semibold text-lg">{{ profileUser.nick_name }}</div>
-                <div class="text-xs text-gray-500">{{ profileUser.num_posts }} 게시글</div>
+                <div class="font-semibold text-lg">{{ userInfo.nick_name }}</div>
+                <div class="text-xs text-gray-500">{{ userInfo.num_posts }} 게시글</div>
             </div>
         </div>
 
         <!-- profile edit button -->
         <div class="text-right mt-2 mr-2 relative">
-            <div v-if="userInfo.uid === profileUser.uid">
+            <div>
                 <button @click="showProfileEditModal = true" class="absolute border-2 border-blue-300 text-sm text-blue-300 px-3 py-2 hover:bg-blue-50 w-24 -bottom-12 right-0 font-bold rounded-full">프로필 수정</button>
-            </div>
-            <div v-else>
-                <div v-if="userInfo.followings.includes(profileUser.uid)" @click="onUnfollow">
-                    <button class="absolute w-24 -bottom-12 right-0 text-sm bg-primary text-white px-3 py-2 hover:opacity-0 font-bold rounded-full">팔로잉</button>
-                    <button class="absolute w-24 -bottom-12 right-0 text-sm bg-red-400 text-white px-3 py-2 opacity-0 hover:opacity-100 font-bold rounded-full">언팔로우</button>
-                </div>
-                <div v-else @click="onFollow">
-                    <button class="absolute w-24 -bottom-12 right-0 border-2 border-blue-300 text-sm text-blue-300 px-3 py-2 hover:bg-blue-50 font-bold rounded-full">팔로우</button>
-                </div>
             </div>
             <div class="absolute -bottom-20 right-0 text-sm mr-4">
                 <button class="mr-4" @click="showFollowingModal = true">
-                    <span class="font-bold mr-1">{{ profileUser.followings.length }}</span>
+                    <span class="font-bold mr-1">{{ userInfo.followings.length }}</span>
                     <span class="text-gray">팔로우 중</span>
                 </button>
                 <button @click="showFollowerModal = true">
-                    <span class="font-bold mr-1">{{ profileUser.followers.length }}</span>
+                    <span class="font-bold mr-1">{{ userInfo.followers.length }}</span>
                     <span class="text-gray">팔로워</span>
                 </button>
             </div>
@@ -41,19 +32,19 @@
         <div class="mx-4 mt-1">
             <!-- porfile image -->
             <div class="border-4 border-white bg-gray-100 w-24 h-24 rounded-full">
-                <img :src="profileUser.profile_image_url" class="rounded-full opacity-90 hover:opacity-100 cursor-pointer w-full h-full object-cover">
+                <img :src="userInfo.profile_image_url" class="rounded-full opacity-90 hover:opacity-100 cursor-pointer w-full h-full object-cover">
             </div>
             <div>
-                <span class="font-extrabold text-lg mr-5">{{ profileUser.nick_name }}</span>
+                <span class="font-extrabold text-lg mr-5">{{ userInfo.nick_name }}</span>
                 <span class="border-2 border-blue-300 px-2 py-1 rounded-full">
                     <i class="fa-solid fa-location-dot text-primary"></i>
-                    {{ profileUser.main_center }}
-                    <i :class="`fa-solid fa-circle ${profileUser.my_level}`"></i>
+                    {{ userInfo.main_center }}
+                    <i :class="`fa-solid fa-circle ${userInfo.my_level}`"></i>
                 </span>
             </div>
             <div class="text-sm mt-1">
                 <span class="text-gray pr-1">가입일 :</span>
-                <span class="text-gray">{{ dayjs(profileUser.created_at).format("YYYY년 MM월 DD일") }}</span>
+                <span class="text-gray">{{ dayjs(userInfo.created_at).format("YYYY년 MM월 DD일") }}</span>
             </div>
         </div>
 
@@ -81,7 +72,7 @@ import ProfileEditModal from '../components/ProfileEditModal.vue'
 import { ref, computed, onBeforeMount, } from 'vue'
 import store from '../store'
 import { PostCollection, LikeCollection, db } from '../firebase'
-import { onSnapshot, orderBy, query, where, doc, getDoc, updateDoc, arrayUnion, arrayRemove, } from 'firebase/firestore'
+import { onSnapshot, orderBy, query, where, doc, getDoc, } from 'firebase/firestore'
 import { useRoute } from 'vue-router'
 import router from '../router'
 import getPostInfo from '../utils/getPostInfo'
@@ -93,7 +84,6 @@ export default {
     components: { Follow, Post, ProfileEditModal, FollowerModal, FollowingModal, },
     setup() {
         const userInfo = computed(() => store.state.user)
-        const profileUser = ref(null)
         const posts = ref([])
         const likePosts = ref([])
         const currentTab = ref("post")
@@ -103,14 +93,9 @@ export default {
         const showFollowingModal = ref(false)
 
         onBeforeMount(() => {
-            const profileUID = route.params.uid ?? userInfo.value.uid
 
-            onSnapshot(doc(db, "users", profileUID), (doc) => {
-                profileUser.value = doc.data()
-            })
-
-            const qPost = query(PostCollection, where("uid", "==", profileUID), orderBy("created_at", "desc"))
-            const qLike = query(LikeCollection, where("uid", "==", profileUID), orderBy("created_at", "desc"))
+            const qPost = query(PostCollection, where("uid", "==", userInfo.value.uid), orderBy("created_at", "desc"))
+            const qLike = query(LikeCollection, where("uid", "==", userInfo.value.uid), orderBy("created_at", "desc"))
         
             onSnapshot(qPost, (snapshot) => {
                 snapshot.docChanges().forEach( async (change) => {
@@ -142,30 +127,6 @@ export default {
             })
         })
 
-        const onFollow = async () => {
-            await updateDoc(doc(db, "users", userInfo.value.uid), {
-                followings: arrayUnion(profileUser.value.uid)
-            })
-
-            await updateDoc(doc(db, "users", profileUser.value.uid), {
-                followers: arrayUnion(userInfo.value.uid)
-            })
-
-            store.commit("setFollow", profileUser.value.uid)
-        }
-
-        const onUnfollow = async () => {
-            await updateDoc(doc(db, "users", userInfo.value.uid), {
-                followings: arrayRemove(profileUser.value.uid)
-            })
-
-            await updateDoc(doc(db, "users", profileUser.value.uid), {
-                followers: arrayRemove(userInfo.value.uid)
-            })
-
-            store.commit("setUnFollow", profileUser.value.uid)
-        }
-
         return {
             userInfo,
             posts,
@@ -173,11 +134,8 @@ export default {
             route,
             dayjs,
             currentTab,
-            profileUser,
             showProfileEditModal,
             router,
-            onFollow,
-            onUnfollow,
             showFollowerModal,
             showFollowingModal,
         }
--- src/pages/UserProfile.vue
+++ src/pages/UserProfile.vue
@@ -104,7 +104,6 @@ export default {
 
         onBeforeMount(() => {
             const profileUID = route.params.uid ?? userInfo.value.uid
-            console.log(profileUID)
 
             onSnapshot(doc(db, "users", profileUID), (doc) => {
                 profileUser.value = doc.data()
